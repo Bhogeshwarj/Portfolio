1dc4794eae60a53c46cda81a19fd5e8088424232
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-04-04 21:46:19 +0530

    Changed the skillCard Section

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -4,7 +4,7 @@ const SkillCard = ({title,value}) => {
   return (
     <div className="flex justify-between items-center mt-5 backdrop-blur-3xl shadow-xl p-3 rounded-2xl border border-slate-600 shadow-gray-1000 mr-2">
       <h3 className="text-xl text-white">{title}</h3>
-      <progress max="100" value={value} className="progress-bar" />
+
     </div>
   )
 }
--- src/Components/SkillSection.jsx
+++ src/Components/SkillSection.jsx
@@ -5,17 +5,33 @@ import SkillsCard from './SkillCard'
 
 const SkillSection = () => {
   return (
-    <div data-aos="fade-right" className="mb-20 lg:mb-36">
-      <h1 className="text-5xl lg:text-7xl font-bold mb-10">My Skills</h1>
-      {/* <div className="h-[300px] overflow-y-scroll select-none scroll-bar"> */}
-        <SkillsCard title={"ReactJs"} value={75} />
-        <SkillsCard title={"NextJs"} value={50} />
-        <SkillsCard title={"Tailwind css"} value={80} />
-        <SkillsCard title={"NodeJs"} value={40} />
-        <SkillsCard title={"MongoDB"} value={35} />
-      {/* </div> */}
-    </div>
+<div data-aos="fade-right" className="mb-20 lg:mb-36">
+  <h1 className=" text-3xl lg:text-5xl font-bold mb-10">My Skills</h1>
+  <div className="flex flex-wrap justify-center items-center gap-2">  
+    <SkillsCard title={"ReactJs"} />
+    <SkillsCard title={"NextJs"} />
+    <SkillsCard title={"Tailwind css"} />
+    <SkillsCard title={"NodeJs"} />
+    <SkillsCard title={"Typescript"} />
+    <SkillsCard title={"Next Js"} />
+    <SkillsCard title={"AWS"} />
+    <SkillsCard title={"Docker"} />
+
+  </div>
+</div>
+
   )
+  //   <div data-aos="fade-right" className="mb-20 lg:mb-36">
+  //     <h1 className="text-5xl lg:text-7xl font-bold mb-10">My Skills</h1>
+  //     {/* <div className="h-[300px] overflow-y-scroll select-none scroll-bar"> */}
+  //       <SkillsCard title={"ReactJs"}  />
+  //       <SkillsCard title={"NextJs"}  />
+  //       <SkillsCard title={"Tailwind css"}  />
+  //       <SkillsCard title={"NodeJs"}  />
+  //       <SkillsCard title={"MongoDB"}  />
+  //     {/* </div> */}
+  //   </div>
+  // )
 }
 
 export default SkillSection
